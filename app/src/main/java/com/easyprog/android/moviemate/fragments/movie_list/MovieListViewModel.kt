08d6046d4c65edf3f496a01f6d1265e585fbae3c
NymiKo <dimon.kabernik8@gmail.com>
2023-03-28 18:08:38 +0500

    refactoring tests for MovieListViewModel

---
+++ app/src/main/java/com/easyprog/android/moviemate/fragments/movie_list/MovieListViewModel.kt
@@ -0,0 +1,16 @@
+package com.easyprog.android.moviemate.fragments.movie_list
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.easyprog.android.moviemate.data.model.Movie
+import com.easyprog.android.moviemate.data.Result
+import com.easyprog.android.moviemate.domain.MovieRepository
+import kotlinx.coroutines.launch
+
+class MovieListViewModel(private val repository: MovieRepository): ViewModel() {
+
+
+
+}
--- app/src/test/java/com/easyprog/android/moviemate/fragments/movie_list/MovieListViewModelTest.kt
+++ app/src/test/java/com/easyprog/android/moviemate/fragments/movie_list/MovieListViewModelTest.kt
@@ -1,13 +1,22 @@
 package com.easyprog.android.moviemate.fragments.movie_list
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.easyprog.android.moviemate.CoroutineTestRule
 import com.easyprog.android.moviemate.data.model.Movie
 import com.easyprog.android.moviemate.data.Result
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert.*
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 
 class MovieListViewModelTest {
+
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    val coroutineTestRule = CoroutineTestRule()
 
     private lateinit var viewModel: MovieListViewModel
     private val repository = FakeMovieListRepository()
@@ -21,20 +30,29 @@ class MovieListViewModelTest {
     fun `get a list of movies and check the result for success`() = runBlocking {
         val expectedMovieList = listOf(
             Movie(0, "Милый дом"),
-            Movie(1, "Завтра"),
-            Movie(2, "Алиса в пограничье"),
-            Movie(3, "Во имя мести")
+            Movie(1, "Завтра")
         )
+        val expectedStateList = listOf(
+            Result.LOADING,
+            Result.SUCCESS(expectedMovieList)
+        )
+        val actualStateList = mutableListOf<Result<List<Movie>>>()
+        viewModel.movieList.observeForever { actualStateList.add(it) }
         repository.setMovieList(expectedMovieList)
-        val actualMovieList = viewModel.getMovieList()
-        assertEquals(expectedMovieList, actualMovieList)
+        assertEquals(expectedStateList, actualStateList)
     }
 
     @Test
     fun `get a list of movies and check the result for an error`() = runBlocking {
+        val expectedStateList = listOf(
+            Result.LOADING,
+            Result.ERROR(Exception())
+        )
+        val actualStateList = mutableListOf<Result<List<Movie>>>()
+        viewModel.movieList.observeForever { actualStateList.add(it) }
         repository.setMovieList(error = true)
-        val actualResult = viewModel.getMovieList()
-        assertTrue(actualResult is Result.ERROR)
+        assertTrue(actualStateList[0] is Result.LOADING)
+        assertTrue(actualStateList[1] is Result.ERROR)
     }
 
 }
