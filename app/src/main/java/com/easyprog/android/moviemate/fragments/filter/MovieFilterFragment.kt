5c8d9bea118fc5df5f674f476741ba4d7a19092f
NymiKo <dimon.kabernik8@gmail.com>
2023-03-30 19:49:09 +0500

    improved code readability in SearchFragment

---
+++ app/src/main/java/com/easyprog/android/moviemate/fragments/filter/MovieFilterFragment.kt
@@ -0,0 +1,32 @@
+package com.easyprog.android.moviemate.fragments.filter
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.easyprog.android.moviemate.R
+import com.easyprog.android.moviemate.databinding.FragmentMovieFilterBinding
+import com.easyprog.android.moviemate.fragments.base.BaseFragment
+
+class MovieFilterFragment :
+    BaseFragment<FragmentMovieFilterBinding>(FragmentMovieFilterBinding::inflate) {
+
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        arguments?.let {
+//            param1 = it.getString(ARG_PARAM1)
+//            param2 = it.getString(ARG_PARAM2)
+//        }
+//    }
+
+    companion object {
+//        @JvmStatic
+//        fun newInstance(param1: String, param2: String) =
+//            MovieFilterFragment().apply {
+//                arguments = Bundle().apply {
+//                    putString(ARG_PARAM1, param1)
+//                    putString(ARG_PARAM2, param2)
+//                }
+//            }
+    }
+}
--- app/src/main/java/com/easyprog/android/moviemate/utils/Extension.kt
+++ app/src/main/java/com/easyprog/android/moviemate/utils/Extension.kt
@@ -1,9 +1,12 @@
 package com.easyprog.android.moviemate.utils
 
+import android.content.Context
+import android.view.inputmethod.InputMethodManager
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.annotation.StringRes
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
 import com.google.android.material.snackbar.Snackbar
 
@@ -17,4 +20,13 @@ fun Fragment.showSnackBar(@StringRes message: Int) {
 
 fun <T> ImageView.loadImage(image: T) {
     Glide.with(this).load(image).fitCenter().centerCrop().into(this)
+}
+
+fun Fragment.hideKeyboard() {
+    val imm = requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.hideSoftInputFromWindow(requireView().windowToken, 0)
+}
+
+fun Fragment.navigateTo(fragment: Int) {
+    findNavController().navigate(fragment)
 }
