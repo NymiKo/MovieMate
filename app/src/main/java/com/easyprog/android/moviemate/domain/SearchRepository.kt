191a6388111bac9b7b2e406af5e0c982c2bc9211
NymiKo <dimon.kabernik8@gmail.com>
2023-04-06 16:16:29 +0500

    implementation of the getRecommendedMovies method by tests

--- app/src/main/java/com/easyprog/android/moviemate/domain/SearchRepository.kt
+++ app/src/main/java/com/easyprog/android/moviemate/domain/SearchRepository.kt
@@ -5,4 +5,5 @@ import com.easyprog.android.moviemate.data.Result
 
 interface SearchRepository {
     suspend fun getMovieListBySearch(searchQuery: String): Result<List<Movie>>
+    suspend fun getRecommendedMovies(): Result<List<Movie>>
 }
