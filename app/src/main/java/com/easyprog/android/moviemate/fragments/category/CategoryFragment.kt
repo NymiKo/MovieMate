ece0ffecb7a8df6e536a903c1675b9521cd9e08f
NymiKo <dimon.kabernik8@gmail.com>
2023-04-07 23:48:26 +0500

    added collapsed toolbar and writing argument from bundle to toolbar title

--- app/src/main/java/com/easyprog/android/moviemate/fragments/category/CategoryFragment.kt
+++ app/src/main/java/com/easyprog/android/moviemate/fragments/category/CategoryFragment.kt
@@ -5,11 +5,21 @@ import android.util.Log
 import android.view.View
 import com.easyprog.android.moviemate.databinding.FragmentCategoryBinding
 import com.easyprog.android.moviemate.fragments.base.BaseFragment
+import com.easyprog.android.moviemate.utils.hideBottomNavView
 
 class CategoryFragment : BaseFragment<FragmentCategoryBinding>(FragmentCategoryBinding::inflate) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Log.e("ARGUMENT", requireArguments().getString("category").toString())
+        setupView()
+    }
+
+    private fun setupView() {
+        hideBottomNavView()
+        setupToolbar()
+    }
+
+    private fun setupToolbar() {
+        binding.collapsingToolbar.title = requireArguments().getString("category")
     }
 }
